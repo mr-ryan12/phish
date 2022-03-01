f9d4429f51af2d74c6e6b82746a30c65bcab9ecb
Ryan McBride <mr.ryan444@gmail.com>
2022-03-01 16:37:47 -0700

    Create show component

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -14,4 +14,20 @@ const fetchYears = () => {
     })
 }
 
-export { fetchYears }
+const fetchYearData = year => {
+  return fetch(`https://phish.in/api/v1/years/${year}.json`, {
+    headers: {
+      'Accept': 'application/json', 
+      'Authorization': 'Bearer a2079c6b7e26152391f7dca8b63851357fa5d24e23ce7a263fd37bece859dd215d1e1fd8effedda6ca91bcff64e2f797'
+    }
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      throw new Error('Something went wrong.')
+    }
+  })
+}
+
+export { fetchYears, fetchYearData }
