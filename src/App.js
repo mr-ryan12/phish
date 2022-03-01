12913693fe7b8ebe7a8fee2e64d6d67033b88978
Ryan McBride <mr.ryan444@gmail.com>
2022-03-01 15:20:04 -0700

    Change year data from dummy to fetch call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { fetchYears } from './apiCalls'
-import PropTypes from 'prop-types'
 import Years from './Years/Years'
 import './App.scss'
 
@@ -8,16 +7,19 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      years: [{date: '1983-1987', show_count: 20}, {date: '1988', show_count: 10}, {date: '1989', show_count: 9}, {date:'1990', show_count: 35}, {date: '1991', show_count: 40}, {date: '1992', show_count: 24}, {date: '1993', show_count: 11}, {date: '1994', show_count: 118}, {date: '1995', show_count: 80}, {date: '1996', show_count: 44}, {date: '1997', show_count: 22}, {date: '1998', show_count: 70}, {date: '1999', show_count: 36}, {date: '2000', show_count: 90}]
+      years: []
     }
   }
 
-  // componentDidMount = () => {
-  //   fetchYears()
-  //     .then(data => {
-  //       console.log(data)
-  //     })
-  // }
+  componentDidMount = () => {
+    fetchYears()
+      .then(data => {
+        this.setState({
+          years: data.data
+        })
+      })
+      .catch(error => console.log(error))
+  }
 
   render() {
     return (
