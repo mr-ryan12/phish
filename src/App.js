fc2bd70c1105feb2750728b1eefb568d640f6535
Ryan McBride <mr.ryan444@gmail.com>
2022-03-01 15:11:04 -0700

    Create dummy data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { fetchYears } from './apiCalls'
 import Years from './Years/Years'
 import './App.scss'
 
@@ -6,9 +7,16 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      years: ['1983-1987', '1988', '1989', '1990', '1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000']
+      years: [{date: '1983-1987', show_count: 20}, {date: '1988', show_count: 10}, {date: '1989', show_count: 9}, {date:'1990', show_count: 35}, {date: '1991', show_count: 40}, {date: '1992', show_count: 24}, {date: '1993', show_count: 11}, {date: '1994', show_count: 118}, {date: '1995', show_count: 80}, {date: '1996', show_count: 44}, {date: '1997', show_count: 22}, {date: '1998', show_count: 70}, {date: '1999', show_count: 36}, {date: '2000', show_count: 90}]
     }
   }
+
+  // componentDidMount = () => {
+  //   fetchYears()
+  //     .then(data => {
+  //       console.log(data)
+  //     })
+  // }
 
   render() {
     return (
--- src/Years/Years.js
+++ src/Years/Years.js
@@ -5,10 +5,10 @@ import './Years.scss'
 const Years = props => {
   const allYears = props.years.map(year => {
     return (
-      <section className="year-card" key={year}>
+      <section className="year-card" key={year.date}>
         <img src={phishLogo} alt="circular-phish-logo"/>
-        <h2>{year}</h2>
-        <p>Total Shows: 20</p>
+        <h2>{year.date}</h2>
+        <p>Total Shows: {year.show_count}</p>
       </section>
     )
   })
