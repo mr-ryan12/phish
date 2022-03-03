7c74d0e61c907552cd1ce89c3948e5df4d195b52
Ryan McBride <mr.ryan444@gmail.com>
2022-03-02 18:22:04 -0700

    Display navigation links after page loads

--- src/NavigationShows/NavigationShows.js
+++ src/NavigationShows/NavigationShows.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import './NavigationShows.scss'
 
-const NavigationShows = () => {
+const checkButtons = loading => {
+  if (!loading) {
+    return (
+      <>
+        <Link to="/">Home</Link>
+        <Link to="/randomShow" className="random-show-link">Random Show</Link>
+      </>
+    )
+  }
+  return null
+}
+
+const NavigationShows = props => {
   return (
     <section className="navigation-container-shows">
       <h1>PHISH</h1>
       <section className="nav-link-container-shows">
-        <Link to="/">Home</Link>
-        <Link to="/randomShow" className="random-show-link">Random Show</Link>
+        {/* <Link to="/">Home</Link>
+        <Link to="/randomShow" className="random-show-link">Random Show</Link> */}
+        {checkButtons(props.isLoading)}
       </section>
     </section>
   )
+}
+
+NavigationShows.propTypes = {
+  isLoading: PropTypes.bool.isRequired
 }
 
 export default NavigationShows
