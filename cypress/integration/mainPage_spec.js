5a0ff7e3988a7f546e44cc37491f82056d8facd9
Ryan McBride <mr.ryan444@gmail.com>
2022-03-03 18:16:12 -0700

    Add fixtures and tests

--- cypress/integration/mainPage_spec.js
+++ cypress/integration/mainPage_spec.js
@@ -7,23 +7,18 @@ describe('Main Page User Flow', () => {
         'Accept': 'application/json', 
         'Authorization': 'Bearer a2079c6b7e26152391f7dca8b63851357fa5d24e23ce7a263fd37bece859dd215d1e1fd8effedda6ca91bcff64e2f797'
       }
-    },{"data": [
-      {
-          "date": "1983-1987",
-          "show_count": 34
-      },
-      {
-          "date": "1988",
-          "show_count": 44
-      },
-      {
-          "date": "1989",
-          "show_count": 64
-      }]})
+    },{ fixture: 'yearCards.json' })
     cy.visit('http://localhost:3000/')
   })
 
-  it('should be true', () => {
-    expect(true).to.equal(true)
+  it('Should have a background image', () => {
+    cy.get('body')
+      .should('have.css', 'background-image', 'url("http://localhost:3000/static/media/backdrop.5a496141d28fb4362490.jpeg")')
+  })
+
+  it('Should have a heading', () => {
+    cy.get('h1')
+      .first()
+      .should('have.text', 'PHISH')
   })
 })
