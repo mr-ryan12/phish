309c4ceb1960da41fd2aaa2ef822de3616425824
Ryan McBride <mr.ryan444@gmail.com>
2022-03-03 20:24:42 -0700

    Add and pass user flow tests for failed fetch

--- cypress/integration/showsPage_spec.js
+++ cypress/integration/showsPage_spec.js
@@ -70,3 +70,61 @@ describe('Shows Page User Flow', () => {
       .should('eq', 'http://localhost:3000/1983-1987/1324')
   });
 });
+
+describe('Shows Page User Flow - Failed Fetch', () => {
+  beforeEach(() => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://phish.in/api/v1/years/1983-1987.json',
+      headers: {
+        'Accept': 'application/json', 
+        'Authorization': 'Bearer a2079c6b7e26152391f7dca8b63851357fa5d24e23ce7a263fd37bece859dd215d1e1fd8effedda6ca91bcff64e2f797'
+      }
+    }, { statusCode: 404 })
+    cy.visit('http://localhost:3000/1983-1987')
+  });
+
+  it('Should have a background image', () => {
+    cy.get('body')
+      .should('have.css', 'background-image', 'url("http://localhost:3000/static/media/backdrop.5a496141d28fb4362490.jpeg")')
+  });
+
+  it('Should have an error message', () => {
+    cy.get('h2')
+      .should('have.text', 'Something went wrong')
+  });
+
+  it('Shoud not display any cards', () => {
+    cy.get('.show-card')
+      .should('not.exist')
+  });
+});
+
+describe('Shows Page User Flow - Incorrect URL Entry', () => {
+  beforeEach(() => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://phish.in/api/v1/years/1983-1987.json',
+      headers: {
+        'Accept': 'application/json', 
+        'Authorization': 'Bearer a2079c6b7e26152391f7dca8b63851357fa5d24e23ce7a263fd37bece859dd215d1e1fd8effedda6ca91bcff64e2f797'
+      }
+    }, { statusCode: 404 })
+    cy.visit('http://localhost:3000/1983-1987')
+  });
+
+  it('Should have a background image', () => {
+    cy.get('body')
+      .should('have.css', 'background-image', 'url("http://localhost:3000/static/media/backdrop.5a496141d28fb4362490.jpeg")')
+  });
+
+  it('Should have an error message', () => {
+    cy.get('h2')
+      .should('have.text', 'Something went wrong')
+  });
+
+  it('Shoud not display any cards', () => {
+    cy.get('.show-card')
+      .should('not.exist')
+  });
+});
