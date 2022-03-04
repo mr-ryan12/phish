96f299575d3e7bba46fd7a4599b9f0e8093e330e
Ryan McBride <mr.ryan444@gmail.com>
2022-03-04 16:36:16 -0700

    Adjust random show page styling for mobile

--- src/RandomShow/RandomShowDisplay.js
+++ src/RandomShow/RandomShowDisplay.js
@@ -2,14 +2,15 @@ import React from 'react'
 import NavigationRandomShow from '../NavigationRandomShow/NavigationRandomShow'
 import PropTypes from 'prop-types'
 import '../ShowDetails/ShowDetails.scss'
+import './RandomShow.scss'
 
 const RandomShowDisplay = props => {
   return (
     <>
       <NavigationRandomShow isLoading={props.isLoading} />
       <section className="show-details-container">
-        <h2 style={{color: 'white'}}>{props.venueName}</h2>
-        <p style={{color: 'white'}}>{props.date}</p>
+        <h2 style={{color: 'white'}} className="random-show-venue-name">{props.venueName}</h2>
+        <p style={{color: 'white'}} className="random-show-date">{props.date}</p>
         <section className="tracks-container">
           {props.renderTracks()}
         </section>
