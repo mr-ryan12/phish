d4b64344326cb82d4b9364295028a0023a1491f9
Ryan McBride <mr.ryan444@gmail.com>
2022-03-15 18:17:31 -0600

    Update variables

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,8 @@
 const fetchData = path => {
   return fetch(`https://phish.in/api/v1/${path}`, {
       headers: {
-        'Accept': 'application/json', 
-        'Authorization': 'Bearer a2079c6b7e26152391f7dca8b63851357fa5d24e23ce7a263fd37bece859dd215d1e1fd8effedda6ca91bcff64e2f797'
+        'Accept': 'application/json',
+        'Authorization': process.env.REACT_APP_API_KEY
       }
     })
     .then(response => {
